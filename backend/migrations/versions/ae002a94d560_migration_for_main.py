a78e86e176cf3b429e7d95ad3df14c2d82bf87e9
Weston Voglesonger <111527221+tvogle@users.noreply.github.com>
2024-03-10 15:40:53 -0400

    Still trying to fix db.

--- backend/migrations/versions/ae002a94d560_migration_for_main.py
+++ backend/migrations/versions/ae002a94d560_migration_for_main.py
@@ -5,12 +5,13 @@ Revises:
 Create Date: 2024-03-10 16:11:03.872596
 
 """
+
 from alembic import op
 import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'ae002a94d560'
+revision = "ae002a94d560"
 down_revision = None
 branch_labels = None
 depends_on = None
